3d6ba943aef3d76194e6bd26cca965b2ad6ef56a
Muhardiansyah <70386390+muhardiansyah15@users.noreply.github.com>
2023-07-16 02:26:05 +0700

    Initial Commit

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -10,19 +10,9 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 	data := []map[string]interface{}{
 		{
-			"id":           1,
-			"nama_product": "Kemeja",
-			"stok":         1000,
-		},
-		{
-			"id":           2,
-			"nama_product": "Celana",
-			"stok":         10000,
-		},
-		{
-			"id":           1,
-			"nama_product": "Sepatu",
-			"stok":         500,
+			"title":  "About Me",
+			"designation": "Software Developer",
+			"description":  "Hello, my name is Muhardiansyah. I am a Software Engineer and hold a Bachelor of Science degree in Mathematics. With over three years of experience as a software engineer at a software house company, I have developed expertise in ERP software development. My focus has been on backend development, where I have acquired proficiency in Accounting, Manufacturing Resource Planning (MRP), Inventory Management, Sales, and Customer Relationship Management (CRM) functionalities within ERP systems. I possess strong programming skills in Python, PostgreSQL, and MySQL, and I am well-versed in web development technologies such as PHP, HTML, and CSS.",
 		},
 	}
 
